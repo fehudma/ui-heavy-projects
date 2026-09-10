a220a35cd8f602c9fe58291dbcce41ad39a9fc95
fehu.dma <fehu.dma@gmail.com>
2026-09-10 18:08:33 +0300

    all audio settings slider signals now can effect the slider

--- settings_menu.gd
+++ settings_menu.gd
@@ -1,6 +1,17 @@
 extends TabContainer
 
 @onready var master_value_label: Label = $Audio/AudioGrid/MasterValueLabel
+@onready var music_value_label: Label = $Audio/AudioGrid/MusicValueLabel
+@onready var sfx_value_label: Label = $Audio/AudioGrid/SFXValueLabel
+
+func _format_percentage(value: float) -> String:
+	return str(int(value)) + "%"
 
 func _on_master_slider_value_changed(value: float) -> void:
-	master_value_label.text = str(int(value)) + "%"
+	master_value_label.text = _format_percentage(value)
+
+func _on_music_slider_value_changed(value: float) -> void:
+	music_value_label.text = _format_percentage(value)
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	sfx_value_label.text = _format_percentage(value)
