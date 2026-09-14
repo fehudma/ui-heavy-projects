b8df57ff8584f0958ddafe3b51895037d9ccdbc0
fehu.dma <fehu.dma@gmail.com>
2026-09-14 14:50:32 +0300

    Capture a key

--- settings_menu.gd
+++ settings_menu.gd
@@ -3,6 +3,26 @@ extends TabContainer
 @onready var master_value_label: Label = $Audio/AudioGrid/MasterValueLabel
 @onready var music_value_label: Label = $Audio/AudioGrid/MusicValueLabel
 @onready var sfx_value_label: Label = $Audio/AudioGrid/SFXValueLabel
+@onready var binding_group: ButtonGroup = \
+	$Controls/BindingsGrid/InteractBindingButton.button_group
+
+func _input(event: InputEvent) -> void:
+	if not event is InputEventKey:
+		return
+
+	var key_event := event as InputEventKey
+
+	if not key_event.pressed or key_event.echo:
+		return
+
+	var active_button := binding_group.get_pressed_button() as Button
+
+	if active_button == null:
+		return
+
+	active_button.text = key_event.as_text_keycode()
+	active_button.button_pressed = false
+	get_viewport().set_input_as_handled()
 
 func _format_percentage(value: float) -> String:
 	return str(int(value)) + "%"
